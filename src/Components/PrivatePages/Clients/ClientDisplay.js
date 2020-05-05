d19b4c9b1976529704228de9ff74864b8da8b215
willmharris <willmharris0@gmail.com>
2020-05-05 02:17:24 -0400

    got fat client model working

--- src/Components/PrivatePages/Clients/ClientDisplay.js
+++ src/Components/PrivatePages/Clients/ClientDisplay.js
@@ -4,15 +4,21 @@ import { NavLink } from 'react-router-dom'
 function ClientDisplay(props) {
     return(
         <div>
-            {props.currentClient ? props.currentClient.first_name : null} {props.currentClient ? props.currentClient.last_name : null}
-            <br />
-            {props.currentClient ? props.currentClient.email : null}
-            <br />
-            Cases:
-            <br />
-            {props.currentClientCases ? 
-                props.currentClientCases.map(theCase => <NavLink to={`/admin/cases/#${theCase.id}`}>{theCase.confirmed_location}</NavLink>) 
-                : 
+            {props.currentClient ?
+                <div>
+                    {props.currentClient.first_name} {props.currentClient.last_name}
+                    <br />
+                    Email: {props.currentClient.email}
+                    <br />
+                    Phone number: {props.currentClient.phone_number}
+                    <br />
+                    Zip code: {props.currentClient.zip_code}
+                    <br />
+                    Cases:
+                    <br />
+                    {props.currentClientCases.map(theCase => <NavLink to={`/admin/cases/#${theCase.id}`}>{theCase.confirmed_location}</NavLink>) }
+                </div>
+                :
                 null
             }
         </div>
--- src/Components/PrivatePages/Clients/ClientEdit.js
+++ src/Components/PrivatePages/Clients/ClientEdit.js
@@ -9,6 +9,8 @@ class ClientEdit extends Component {
           firstName: null,
           lastName: null,
           email: null,
+          phoneNumber: null,
+          zipCode: null,
           redirect: false
         }
     }
@@ -22,7 +24,11 @@ class ClientEdit extends Component {
             this.setState({lastName: input})
         } else if (id === "email") {
             this.setState({email: input})
-        }
+        } else if (id === "phoneNumber") {
+            this.setState({phoneNumber: input})
+        } else if (id === "zipCode") {
+            this.setState({zipCode: input})
+        }  
     }
 
     editClient = (event) => {
@@ -30,7 +36,9 @@ class ClientEdit extends Component {
         let data = {
             first_name: this.state.firstName,
             last_name: this.state.lastName,
-            email: this.state.email
+            email: this.state.email,
+            phone_number: this.state.phoneNumber,
+            zip_code: this.state.zipCode 
         }
         fetch(`http://localhost:3000/users/${this.props.currentClient.id}`, {
             method: "PATCH", 
@@ -57,8 +65,16 @@ class ClientEdit extends Component {
                     <input type="text" id="firstName" value={this.state.firstName} onChange={this.updateFormState}></input>
                     <label>Last Name:</label>
                     <input type="text" id="lastName" value={this.state.lastName} onChange={this.updateFormState}></input>
+                    <br />
                     <label>Email:</label>
                     <input type="text" id="email" value={this.state.email} onChange={this.updateFormState}></input>
+                    <br />
+                    <label>Phone Number:</label>
+                    <input type="number" id="phoneNumber" value={this.state.phoneNumber} onChange={this.updateFormState}></input>
+                    <br />
+                    <label>Zip Code:</label>
+                    <input type="number" id="zipCode" value={this.state.zipCode} onChange={this.updateFormState}></input>
+                    <br />
                     <input type="submit" onClick={this.editClient}></input>
                 </form>
             </div>
@@ -67,7 +83,13 @@ class ClientEdit extends Component {
 
     componentDidMount() {
         let client = this.props.currentClient
-        this.setState({firstName: client.first_name, lastName: client.last_name, email: client.email})
+        this.setState({
+            firstName: client.first_name, 
+            lastName: client.last_name, 
+            email: client.email,
+            phoneNumber: client.phone_number,
+            zipCode: client.zip_code 
+        })
     }
 }
 
