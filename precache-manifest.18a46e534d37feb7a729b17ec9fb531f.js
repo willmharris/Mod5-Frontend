cf1863b4f58dca9b4914a0c84ead54ce566ac1e6
willmharris <willmharris0@gmail.com>
2020-07-27 10:37:11 -0400

    Updates

---
+++ precache-manifest.18a46e534d37feb7a729b17ec9fb531f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea606cc8610f9136a247eda1bdb980b",
+    "url": "/Mod5-Frontend/index.html"
+  },
+  {
+    "revision": "e2eba6d583d44776ee25",
+    "url": "/Mod5-Frontend/static/css/main.70700e87.chunk.css"
+  },
+  {
+    "revision": "7f84471127efcc5f358d",
+    "url": "/Mod5-Frontend/static/js/2.eca16027.chunk.js"
+  },
+  {
+    "revision": "33ad966c50f3e315fdc82eaebd9dfc8e",
+    "url": "/Mod5-Frontend/static/js/2.eca16027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2eba6d583d44776ee25",
+    "url": "/Mod5-Frontend/static/js/main.2f93b40b.chunk.js"
+  },
+  {
+    "revision": "49cb6e109651993d48d8",
+    "url": "/Mod5-Frontend/static/js/runtime-main.33737f0a.js"
+  }
+]);
